3067c18b1065094283f82778f6f2cccb6ae830a9
zhangzewen <daixiuhua_1989@163.com>
2013-01-22 17:29:48 -0800

    add fn

--- src/event/signal.c
+++ src/event/signal.c
@@ -15,10 +15,68 @@ int Sigemptyset(sigset_t *set)
 	}
 	return 0;
 }
-int Sigfillset(sigset_t *set);
-int Sigaddset(sigset_t *set, int signum);
-int Sigdelset(sigset_t *set, int signum);
-int Sigismember(const sigset_t *set, int signum);
-int Sigprocmask(int how, const sigset_t *set, sigset_t *oldset);
-int Kill();
-int Raise();
+int Sigfillset(sigset_t *set)
+{
+	if(sigfillset(set) < 0)
+	{
+		err_sys("error");
+	}
+	return 0;
+}
+int Sigaddset(sigset_t *set, int signum)
+{
+	if(sigaddset(set, signum) < 0)
+	{
+		err_sys("error");
+	}
+	return 0;
+}
+int Sigdelset(sigset_t *set, int signum)
+{
+	if(sigdelset(set, signum) < 0)
+	{
+		err_sys("error");
+	}
+	return 0;
+}
+int Sigismember(const sigset_t *set, int signum)
+{
+	int ret = 0;
+	ret = sigismember(set, signum);
+	if(ret == 1)
+	{
+		printf("%d is a member of set", signum);
+		return 1;
+	}else if (ret == 0)
+	{
+		printf("%d is not a member of set", signum);
+		return 0;
+	}else{
+		err_sys("error");
+	}
+	return -1;
+}
+int Sigprocmask(int how, const sigset_t *set, sigset_t *oldset)
+{
+	if(sigprocmask(how, set, oldset) < 0)
+	{
+		err_sys("error");
+	}
+	return 0;
+}
+int Kill(pid_t pid, int sig)
+{
+	if(kill(pid, sig) < 0)
+	{
+		err_sys("error");
+	}
+	return 0;
+}
+int Raise(int sig)
+{
+	if(raise(sig) != 0)
+	{
+		err_sys("error");
+	}
+	return 0;
+}
--- src/event/signal.h
+++ src/event/signal.h
@@ -1,6 +1,5 @@
 #ifndef _HTTP_SIGNAL_H_INCLUDED
 #define _HTTP_SIGNAL_H_INCLUDED
-#ifndef _HTTP_SIGNAL_H_INCLUDED
 
 int Sigacton(int signo, const struct sigaction *restrict act, struct sigaction *restrict oact);
 int Sigemptyset(sigset_t *set);
@@ -9,8 +8,9 @@ int Sigaddset(sigset_t *set, int signum);
 int Sigdelset(sigset_t *set, int signum);
 int Sigismember(const sigset_t *set, int signum);
 int Sigprocmask(int how, const sigset_t *set, sigset_t *oldset);
-int Kill();
-int Raise();
+int Kill(pid_t pid, int sig);
+int Raise(int sig);
+
 int
 
 #endif
